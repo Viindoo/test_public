dc54582db701f0777687c643a1e79089a22a5526
AnhBT <anhbtit@gmail.com>
2022-03-29 09:00:53 +0700

    [ADD] viin_mart, viin_fast: New modules for test

---
+++ viin_mart/__manifest__.py
@@ -0,0 +1,42 @@
+{
+    'name': "ViinMart",
+    'name_vi_VN': "ViinMart",
+    'summary': "Module for Supermarkets",
+    'summary_vi_VN': "Phân hệ cho siêu thị",
+    'description': """
+        What it does
+        ============
+        Key Features
+        ============
+        Supported Editions
+        ==================
+        1. Community Edition
+        2. Enterprise Edition
+    """,
+    'description_vi_VN': """
+        Mô tả
+        =====
+        Tính năng nổi bật
+        =================
+        Ấn bản được Hỗ trợ
+        ==================
+        1. Ấn bản Community
+        2. Ấn bản Enterprise
+    """,
+    'author': 'Viindoo',
+    'website': 'https://viindoo.com',
+    'live_test_url': "https://v14demo-int.viindoo.com",
+    'support': 'apps.support@viindoo.com',
+    'category': "Supermarkets",
+    'version': '0.1',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'images': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'price': 99.0,
+    'currency': 'EUR',
+    'license': 'LGPL-3',
+}
