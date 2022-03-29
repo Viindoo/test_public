3aabb2e4bc7d846ec1dcbfd3d4b47ddd9e8d310b
AnhBT <anhbtit@gmail.com>
2022-03-29 17:53:16 +0700

    [FIX] viin_mart, viin_smart: Format manifest

--- viin_smart/__manifest__.py
+++ viin_smart/__manifest__.py
@@ -4,24 +4,28 @@
     'summary': "Module for phone industry",
     'summary_vi_VN': "Phân hệ cho ngành công nghiệp điện thoại",
     'description': """
-        What it does
-        ============
-        Key Features
-        ============
-        Supported Editions
-        ==================
-        1. Community Edition
-        2. Enterprise Edition
+What it does
+============
+
+Key Features
+============
+
+Supported Editions
+==================
+1. Community Edition
+2. Enterprise Edition
     """,
     'description_vi_VN': """
-        Mô tả
-        =====
-        Tính năng nổi bật
-        =================
-        Ấn bản được Hỗ trợ
-        ==================
-        1. Ấn bản Community
-        2. Ấn bản Enterprise
+Mô tả
+=====
+
+Tính năng nổi bật
+=================
+
+Ấn bản được Hỗ trợ
+==================
+1. Ấn bản Community
+2. Ấn bản Enterprise
     """,
     'author': 'Viindoo',
     'website': 'https://viindoo.com',
